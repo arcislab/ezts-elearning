3dc71b26e7856d1a993a03223f893cbcaa0bb827
arcislab <111510801+arcislab@users.noreply.github.com>
2024-12-10 22:22:38 +0530

    Create Database.php

---
+++ api/config/Database.php
@@ -0,0 +1,15 @@
+<?php
+class Database {
+    private static $connection;
+
+    public static function connect() {
+        if (!self::$connection) {
+            self::$connection = new mysqli('localhost', 'root', 'Phpmy@dm1n', 'ezts_elearn');
+            if (self::$connection->connect_error) {
+                die('Database connection failed: ' . self::$connection->connect_error);
+            }
+        }
+        return self::$connection;
+    }
+}
+?>
